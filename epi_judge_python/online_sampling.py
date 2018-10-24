96ca132bfbe035f7fdf67d6d5356d7d5ea42f69c
Brandon Wong <bwong7@hawaii.edu>
2018-10-23 15:34:38 -1000

    5.13

--- epi_judge_python/online_sampling.py
+++ epi_judge_python/online_sampling.py
@@ -1,4 +1,6 @@
 import functools
+import itertools
+import random
 
 from test_framework import generic_test
 from test_framework.random_sequence_checker import (
@@ -9,8 +11,15 @@ from test_framework.test_utils import enable_executor_hook
 
 # Assumption: there are at least k elements in the stream.
 def online_random_sample(stream, k):
-    # TODO - you fill in here.
-    return []
+    sampling_results = list(itertools.islice(stream, k))
+
+    num_seen_so_far = k
+    for x in stream:
+        num_seen_so_far += 1
+        idx_to_replace = random.randrange(num_seen_so_far)
+        if idx_to_replace < k:
+            sampling_results[idx_to_replace] = x
+    return sampling_results
 
 
 @enable_executor_hook
