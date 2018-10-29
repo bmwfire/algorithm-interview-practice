6190c414f560114a3e6bf746b08b883043e7868f
Brandon Wong <bwong7@hawaii.edu>
2018-10-29 07:04:31 -1000

    6.5

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -2,8 +2,24 @@ from test_framework import generic_test
 
 
 def is_palindrome(s):
-    # TODO - you fill in here.
+    i, j = 0, len(s) - 1
+    while i < j:
+        while not s[i].isalnum() and i < j:
+            i += 1
+        while not s[j].isalnum() and i < j:
+            j -= 1
+        if s[i].lower() != s[j].lower():
+            return False
+        i, j = i + 1, j - 1
     return True
+
+
+# Pythonic Solution:
+def is_palindrome_pythonic(s):
+    return all(a == b
+               for a, b in zip(
+                    map(str.lower, filter(str.isalnum, s)),
+                    map(str.lower, filter(str.isalnum, reversed(s)))))
 
 
 if __name__ == '__main__':
