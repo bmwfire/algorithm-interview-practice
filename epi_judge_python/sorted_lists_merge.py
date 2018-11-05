a2012afc9e10033e2971f5cb76fcb0184cc35205
Brandon Wong <bwong7@hawaii.edu>
2018-11-05 06:57:15 -1000

    Revert "7.1 Merge two sorted lists"
    This reverts commit fb0305bbfa17dc2e05c561f1f51aca39e6c0b323.

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -1,23 +1,9 @@
 from test_framework import generic_test
 
-class ListNode:
-    def __init__(self, data=0, next=None):
-        self.data = data
-        self.next = next
 
 def merge_two_sorted_lists(L1, L2):
-
-    dummy_head = tail = ListNode()
-
-    while L1 and L2:
-        if L1.data < L2.data:
-            tail.next, L1 = L1, L1.next
-        else:
-            tail.next, L2 = L2, L2.next
-        tail = tail.next
-
-    tail.next = L1 or L2
-    return dummy_head.next
+    # TODO - you fill in here.
+    return None
 
 
 if __name__ == '__main__':
