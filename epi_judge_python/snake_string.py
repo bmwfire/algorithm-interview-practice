04bf89618de0ea26ae8aeb51bab94819fd6db8b3
Brandon Wong <bwong7@hawaii.edu>
2018-11-05 06:03:19 -1000

    6.11

--- epi_judge_python/snake_string.py
+++ epi_judge_python/snake_string.py
@@ -2,8 +2,18 @@ from test_framework import generic_test
 
 
 def snake_string(s):
-    # TODO - you fill in here.
-    return ''
+    result = []
+    for i in range(1, len(s), 4):
+        result.append(s[i])
+    for i in range(0, len(s), 2):
+        result.append(s[i])
+    for i in range(3, len(s), 4):
+        result.append(s[i])
+    return ''.join(result)
+
+
+def snake_string_pythonic(s):
+    return s[1::4] + s[::2] + s[3::4]
 
 
 if __name__ == '__main__':
