44c611ecabf4c2748ad95c3eccc48c47f706f20f
Brandon Wong <bwong7@hawaii.edu>
2018-11-07 17:37:38 -1000

    7.8

--- epi_judge_python/remove_duplicates_from_sorted_list.py
+++ epi_judge_python/remove_duplicates_from_sorted_list.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def remove_duplicates(L):
-    # TODO - you fill in here.
-    return None
+    it = L
+    while it:
+        next_distinct = it.next
+        while next_distinct and next_distinct.data == it.data:
+            next_distinct = next_distinct.next
+        it.next = next_distinct
+        it = next_distinct
+    return L
 
 
 if __name__ == '__main__':
