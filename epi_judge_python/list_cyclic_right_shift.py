05712ea39b5a18e7e5eb614c48e45db465c34ef6
Brandon Wong <bwong7@hawaii.edu>
2018-11-07 18:02:58 -1000

    7.9

--- epi_judge_python/list_cyclic_right_shift.py
+++ epi_judge_python/list_cyclic_right_shift.py
@@ -2,8 +2,27 @@ from test_framework import generic_test
 
 
 def cyclically_right_shift_list(L, k):
-    # TODO - you fill in here.
-    return None
+    if not L:
+        return L
+
+    tail, n = L, 1
+    while tail.next:
+        n += 1
+        tail = tail.next
+
+    k %= n
+    if k == 0:
+        return L
+
+    tail.next = L
+    steps_to_new_head, new_tail = n - k, tail
+    while steps_to_new_head:
+        steps_to_new_head -= 1
+        new_tail = new_tail.next
+
+    new_head = new_tail.next
+    new_tail.next = None
+    return new_head
 
 
 if __name__ == '__main__':
