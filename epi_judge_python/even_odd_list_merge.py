bd1bff441a696cbe5e186f153b878386a2a71c31
Brandon Wong <bwong7@hawaii.edu>
2018-11-07 22:13:45 -1000

    7.10

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -1,9 +1,21 @@
+from list_node import ListNode
 from test_framework import generic_test
 
 
 def even_odd_merge(L):
-    # TODO - you fill in here.
-    return None
+    if not L:
+        return L
+
+    even_dummy_head, odd_dummy_head = ListNode(), ListNode()
+    tails, turn = [even_dummy_head, odd_dummy_head], 0
+    while L:
+        tails[turn].next = L
+        L = L.next
+        tails[turn] = tails[turn].next
+        turn ^= 1
+    tails[1].next = None
+    tails[0].next = odd_dummy_head.next
+    return even_dummy_head.next
 
 
 if __name__ == '__main__':
