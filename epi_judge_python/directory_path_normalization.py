d9c9bfc0307348df109f6b72bae66abf1db2f574
Brandon Wong <bwong7@hawaii.edu>
2018-11-11 09:52:12 -1000

    8.4

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -2,8 +2,25 @@ from test_framework import generic_test
 
 
 def shortest_equivalent_path(path):
-    # TODO - you fill in here.
-    return ''
+    if not path:
+        raise ValueError('Empty string is not a valid path.')
+
+    path_names = []
+    if path[0] == '/':
+        path_names.append('/')
+
+    for token in (token for token in path.split('/') if token not in ['.', '']):
+        if token == '..':
+            if not path_names or path_names[-1] == '..':
+                path_names.append(token)
+            else:
+                if path_names[-1] == '/':
+                    raise ValueError('Path error')
+                path_names.pop()
+        else:
+            path_names.append(token)
+    result = '/'.join(path_names)
+    return result[result.startswith('//'):]
 
 
 if __name__ == '__main__':
