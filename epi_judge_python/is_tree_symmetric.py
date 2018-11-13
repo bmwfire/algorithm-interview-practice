71628ad0f127edf6316196391b5ed4ead0246f12
Brandon Wong <bwong7@hawaii.edu>
2018-11-13 02:32:35 -1000

    Revert "9.2"
    This reverts commit 0b2689ef83805e74c3e3a9e0969956c13bcc3b7b.

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -2,16 +2,8 @@ from test_framework import generic_test
 
 
 def is_symmetric(tree):
-    def check_symmetric(subtree_0, subtee_1):
-        if not subtree_0 and not subtee_1:
-            return True
-        elif subtree_0 and subtee_1:
-            return (subtree_0.data == subtee_1.data
-                    and check_symmetric(subtree_0.left, subtee_1.right)
-                    and check_symmetric(subtree_0.right, subtee_1.left))
-        return False
-
-    return not tree or check_symmetric(tree.left, tree.right)
+    # TODO - you fill in here.
+    return True
 
 
 if __name__ == '__main__':
