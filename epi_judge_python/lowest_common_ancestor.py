41d43f69f3cb007cc7802a3661316b3b7b8709bf
Brandon Wong <bwong7@hawaii.edu>
2018-11-15 11:38:29 -1000

    9.3

--- epi_judge_python/lowest_common_ancestor.py
+++ epi_judge_python/lowest_common_ancestor.py
@@ -1,4 +1,5 @@
 import functools
+import collections
 
 from test_framework import generic_test
 from test_framework.binary_tree_utils import must_find_node, strip_parent_link
@@ -7,8 +8,24 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def lca(tree, node0, node1):
-    # TODO - you fill in here.
-    return None
+    Status = collections.namedtuple('Status', ('num_target_nodes', 'ancestor'))
+
+    def lca_helper(tree, node0, node1):
+        if not tree:
+            return Status(0, None)
+
+        left_result = lca_helper(tree.left, node0, node1)
+        if left_result.num_target_nodes == 2:
+            return left_result
+
+        right_result = lca_helper(tree.right, node0, node1)
+        if right_result.num_target_nodes == 2:
+            return right_result
+
+        num_target_nodes = (left_result.num_target_nodes + right_result.num_target_nodes + (node0, node1).count(tree))
+        return Status(num_target_nodes, tree if num_target_nodes == 2 else None)
+
+    return lca_helper(tree, node0, node1).ancestor
 
 
 @enable_executor_hook
