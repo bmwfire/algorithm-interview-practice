bc86652215529de43d1ac917bcf83f359ccd946e
Brandon Wong <bwong7@hawaii.edu>
2018-11-19 00:46:57 -1000

    9.7

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree):
-    # TODO - you fill in here.
-    return []
+    s, result = [], []
+
+    while s or tree:
+        if tree:
+            s.append(tree)
+            tree = tree.left
+        else:
+            tree = s.pop()
+            result.append(tree.data)
+            tree = tree.right
+    return result
 
 
 if __name__ == '__main__':
