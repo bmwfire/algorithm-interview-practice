25110e478c6e8582c4eee7d83d6848b3ac246e4a
Brandon Wong <bwong7@hawaii.edu>
2018-11-20 06:58:56 -1000

    9.11

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -2,8 +2,22 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree):
-    # TODO - you fill in here.
-    return []
+    prev, result = None, []
+    while tree:
+        if prev is tree.parent:
+            if tree.left:
+                next = tree.left
+            else:
+                result.append(tree.data)
+                next = tree.right or tree.parent
+        elif tree.left is prev:
+            result.append(tree.data)
+            next = tree.right or tree.parent
+        else:
+            next = tree.parent
+
+        prev, tree = tree, next
+    return result
 
 
 if __name__ == '__main__':
