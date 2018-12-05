6baeb6b32ebe6169efb5f0ade572fb9161a7c1eb
Brandon Wong <bwong7@hawaii.edu>
2018-12-05 12:49:38 -1000

    10.5

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -1,9 +1,21 @@
+import heapq
+
 from test_framework import generic_test
 
 
 def online_median(sequence):
-    # TODO - you fill in here.
-    return []
+    min_heap = []
+    max_heap = []
+    result = []
+
+    for x in sequence:
+        heapq.heappush(max_heap, -heapq.heappushpop(min_heap, x))
+        if len(max_heap) > len(min_heap):
+            heapq.heappush(min_heap, -heapq.heappop(max_heap))
+
+        result.append(0.5 * (min_heap[0] + (-max_heap[0])) if len(min_heap) == len(max_heap) else min_heap[0])
+
+    return result
 
 
 def online_median_wrapper(sequence):
