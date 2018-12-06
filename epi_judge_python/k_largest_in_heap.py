8448db341ce22ade2377e4ecc2fea97e1102d972
Brandon Wong <bwong7@hawaii.edu>
2018-12-06 11:14:05 -1000

    10.6

--- epi_judge_python/k_largest_in_heap.py
+++ epi_judge_python/k_largest_in_heap.py
@@ -1,9 +1,29 @@
+import heapq
+
 from test_framework import generic_test, test_utils
 
 
 def k_largest_in_binary_heap(A, k):
-    # TODO - you fill in here.
-    return []
+    if k <= 0:
+        return []
+
+    candidates_max_heap = []
+    candidates_max_heap.append((-A[0], 0))
+    result = []
+
+    for _ in range(k):
+        candidate_idx = candidates_max_heap[0][1]
+        result.append(-heapq.heappop(candidates_max_heap)[0])
+
+        left_child_idx = 2 * candidate_idx + 1
+        if left_child_idx < len(A):
+            heapq.heappush(candidates_max_heap, (-A[left_child_idx], left_child_idx))
+
+        right_child_idx = 2 * candidate_idx + 2
+        if right_child_idx < len(A):
+            heapq.heappush(candidates_max_heap, (-A[right_child_idx], right_child_idx))
+
+    return result
 
 
 if __name__ == '__main__':
