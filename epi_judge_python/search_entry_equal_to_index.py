11a26aa914f5cfdb0ee2b4aacf36b21462666f54
Brandon Wong <bwong7@hawaii.edu>
2018-12-16 18:16:35 -1000

    11.2

--- epi_judge_python/search_entry_equal_to_index.py
+++ epi_judge_python/search_entry_equal_to_index.py
@@ -6,8 +6,17 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def search_entry_equal_to_its_index(A):
-    # TODO - you fill in here.
-    return 0
+    left, right = 0, len(A) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        difference = A[mid] - mid
+        if difference == 0:
+            return mid
+        elif difference > 0:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
 
 
 @enable_executor_hook
