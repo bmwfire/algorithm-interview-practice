2f51190eede3eb5af4910c665c549346fd56eb81
Brandon Wong <bwong7@hawaii.edu>
2018-12-22 11:34:04 -0800

    11.8

--- epi_judge_python/kth_largest_in_array.py
+++ epi_judge_python/kth_largest_in_array.py
@@ -1,3 +1,6 @@
+import operator
+import random
+
 from test_framework import generic_test
 
 
@@ -5,8 +8,30 @@ from test_framework import generic_test
 # find_kth_largest(1, A) returns 3, find_kth_largest(2, A) returns 2,
 # find_kth_largest(3, A) returns 1, and find_kth_largest(4, A) returns -1.
 def find_kth_largest(k, A):
-    # TODO - you fill in here.
-    return 0
+    def find_kth(comp):
+        def partition_around_pivot(left, right, pivot_idx):
+            pivot_value = A[pivot_idx]
+            new_pivot_idx = left
+            A[pivot_idx], A[right] = A[right], A[pivot_idx]
+            for i in range(left, right):
+                if comp(A[i], pivot_value):
+                    A[i], A[new_pivot_idx] = A[new_pivot_idx], A[i]
+                    new_pivot_idx += 1
+            A[right], A[new_pivot_idx] = A[new_pivot_idx], A[right]
+            return new_pivot_idx
+
+        left, right = 0, len(A) - 1
+        while left <= right:
+            pivot_idx = random.randint(left, right)
+            new_pivot_idx = partition_around_pivot(left, right, pivot_idx)
+            if new_pivot_idx == k - 1:
+                return A[new_pivot_idx]
+            elif new_pivot_idx > k - 1:
+                right = new_pivot_idx - 1
+            else:
+                left = new_pivot_idx + 1
+
+    return find_kth(operator.gt)
 
 
 if __name__ == '__main__':
