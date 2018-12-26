71e87e17916ac8f90b0d44d6739a48d4a910b9ea
Brandon Wong <bwong7@hawaii.edu>
2018-12-26 02:53:46 -1000

    12.2

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,9 +1,25 @@
+import collections
+
 from test_framework import generic_test
 
 
 def is_letter_constructible_from_magazine(letter_text, magazine_text):
-    # TODO - you fill in here.
-    return True
+    char_frequency_for_letter = collections.Counter(letter_text)
+
+    for c in magazine_text:
+        if c in char_frequency_for_letter:
+            char_frequency_for_letter[c] -= 1
+            if char_frequency_for_letter[c] == 0:
+                del char_frequency_for_letter[c]
+                if not char_frequency_for_letter:
+                    return True
+
+    return not char_frequency_for_letter
+
+
+# Pythonic Solution
+def is_letter_constructible_from_magazine_pythonic(letter_text, magazine_text):
+    return (not collections.Counter(letter_text) - collections.Counter(magazine_text))
 
 
 if __name__ == '__main__':
