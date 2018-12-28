88bf1ff815175a9045e3ae6561972ad9ca59d3f1
Brandon Wong <bwong7@hawaii.edu>
2018-12-28 11:52:17 -1000

    12.8

--- epi_judge_python/longest_subarray_with_distinct_values.py
+++ epi_judge_python/longest_subarray_with_distinct_values.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def longest_subarray_with_distinct_entries(A):
-    # TODO - you fill in here.
-    return 0
+    most_recent_occurrence = {}
+    longest_dup_free_subarray_start_idx = result = 0
+    for i, a in enumerate(A):
+        if a in most_recent_occurrence:
+            dup_idx = most_recent_occurrence[a]
+            if dup_idx >= longest_dup_free_subarray_start_idx:
+                result = max(result, i - longest_dup_free_subarray_start_idx)
+                longest_dup_free_subarray_start_idx = dup_idx + 1
+        most_recent_occurrence[a] = i
+    return max(result, len(A) - longest_dup_free_subarray_start_idx)
 
 
 if __name__ == '__main__':
