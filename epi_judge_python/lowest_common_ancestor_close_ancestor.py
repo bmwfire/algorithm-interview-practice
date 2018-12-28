ab49f8e7d5c5656297c2bdfb41b11f12cf0f8f54
Brandon Wong <bwong7@hawaii.edu>
2018-12-27 23:23:52 -1000

    12.4

--- epi_judge_python/lowest_common_ancestor_close_ancestor.py
+++ epi_judge_python/lowest_common_ancestor_close_ancestor.py
@@ -7,8 +7,20 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def lca(node0, node1):
-    # TODO - you fill in here.
-    return None
+    iter0, iter1 = node0, node1
+    nodes_on_path_to_root = set()
+    while iter0 or iter1:
+        if iter0:
+            if iter0 in nodes_on_path_to_root:
+                return iter0
+            nodes_on_path_to_root.add(iter0)
+            iter0 = iter0.parent
+        if iter1:
+            if iter1 in nodes_on_path_to_root:
+                return iter1
+            nodes_on_path_to_root.add(iter1)
+            iter1 = iter1.parent
+    raise ValueError('node0 and node1 are not in the same tree')
 
 
 @enable_executor_hook
