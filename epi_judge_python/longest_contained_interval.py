fd07f59bf4d114e10265d33f6cc699b9de6ab632
Brandon Wong <bwong7@hawaii.edu>
2018-12-28 12:51:11 -1000

    12.9

--- epi_judge_python/longest_contained_interval.py
+++ epi_judge_python/longest_contained_interval.py
@@ -2,8 +2,24 @@ from test_framework import generic_test
 
 
 def longest_contained_range(A):
-    # TODO - you fill in here.
-    return 0
+    unprocessed_entries = set(A)
+
+    max_interval_size = 0
+    while unprocessed_entries:
+        a = unprocessed_entries.pop()
+
+        lower_bound = a - 1
+        while lower_bound in unprocessed_entries:
+            unprocessed_entries.remove(lower_bound)
+            lower_bound -= 1
+
+        upper_bound = a + 1
+        while upper_bound in unprocessed_entries:
+            unprocessed_entries.remove(upper_bound)
+            upper_bound += 1
+
+        max_interval_size = max(max_interval_size, upper_bound - lower_bound - 1)
+    return max_interval_size
 
 
 if __name__ == '__main__':
