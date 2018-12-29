e7677dccf03e6502765e23657378dc2fcf3ae3db
Brandon Wong <bwong7@hawaii.edu>
2018-12-29 10:22:33 -1000

    13.1

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -1,9 +1,29 @@
+import bisect
+
 from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A, B):
-    # TODO - you fill in here.
-    return []
+    i, j, intersection_A_B = 0, 0, []
+    while i < len(A) and j < len(B):
+        if A[i] == B[j]:
+            if i == 0 or A[i] != A[i - 1]:
+                intersection_A_B.append(A[i])
+            i, j = i + 1, j + 1
+        elif A[i] < B[j]:
+            i += 1
+        else:
+            j += 1
+    return intersection_A_B
+
+    # non-optimal solution using binary search
+    # solution yields time complexity of O(m log n) vs above O(m + n)
+    # def is_present(k):
+    #     i = bisect.bisect_left(B, k)
+    #     return i < len(B) and B[i] == k
+    #
+    # return [a for i, a in enumerate(A)
+    #         if (i == 0 or a != A[i - 1]) and is_present(a)]
 
 
 if __name__ == '__main__':
