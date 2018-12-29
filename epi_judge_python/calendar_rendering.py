537be2767d16f50d01b088af77b95b23b20610bd
Brandon Wong <bwong7@hawaii.edu>
2018-12-29 12:33:45 -1000

    13.6

--- epi_judge_python/calendar_rendering.py
+++ epi_judge_python/calendar_rendering.py
@@ -7,10 +7,23 @@ from test_framework.test_utils import enable_executor_hook
 # Event is a tuple (start_time, end_time)
 Event = collections.namedtuple('Event', ('start', 'finish'))
 
+Endpoint = collections.namedtuple('Endpoint', ('time', 'is_start'))
+
 
 def find_max_simultaneous_events(A):
-    # TODO - you fill in here.
-    return 0
+    E = [p for event in A for p in
+         (Endpoint(event.start, True), Endpoint(event.finish, False))]
+
+    E.sort(key=lambda e: (e.time, not e.is_start))
+
+    max_num_simultaneous_events, num_simultaneous_events = 0, 0
+    for e in E:
+        if e.is_start:
+            num_simultaneous_events += 1
+            max_num_simultaneous_events = max(max_num_simultaneous_events, num_simultaneous_events)
+        else:
+            num_simultaneous_events -= 1
+    return max_num_simultaneous_events
 
 
 @enable_executor_hook
