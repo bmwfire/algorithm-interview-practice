c4fbe9ee70766b82c46d7e61f554f073cd0f9d91
Brandon Wong <bwong7@hawaii.edu>
2018-12-29 12:06:02 -1000

    13.4

--- epi_judge_python/remove_duplicates.py
+++ epi_judge_python/remove_duplicates.py
@@ -8,6 +8,9 @@ class Name:
     def __init__(self, first_name, last_name):
         self.first_name, self.last_name = first_name, last_name
 
+    def __eq__(self, other):
+        return self.first_name == other.first_name
+
     def __lt__(self, other):
         return (self.first_name < other.first_name
                 if self.first_name != other.first_name else
@@ -15,8 +18,13 @@ class Name:
 
 
 def eliminate_duplicate(A):
-    # TODO - you fill in here.
-    return
+    A.sort()
+    write_idx = 1
+    for cand in A[1:]:
+        if cand != A[write_idx - 1]:
+            A[write_idx] = cand
+            write_idx += 1
+    del A[write_idx:]
 
 
 @enable_executor_hook
