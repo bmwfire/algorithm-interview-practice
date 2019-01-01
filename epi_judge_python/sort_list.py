9e06c696060ccd002c9d855b67d02e91e41e8e6f
Brandon Wong <bwong7@hawaii.edu>
2019-01-01 03:24:56 -1000

    13.11

--- epi_judge_python/sort_list.py
+++ epi_judge_python/sort_list.py
@@ -1,9 +1,18 @@
+from sorted_lists_merge import merge_two_sorted_lists
+
 from test_framework import generic_test
 
 
 def stable_sort_list(L):
-    # TODO - you fill in here.
-    return None
+    if not L or not L.next:
+        return L
+
+    pre_slow, slow, fast = None, L, L
+    while fast and fast.next:
+        pre_slow = slow
+        fast, slow = fast.next.next, slow.next
+    pre_slow.next = None
+    return merge_two_sorted_lists(stable_sort_list(L), stable_sort_list(slow))
 
 
 if __name__ == '__main__':
