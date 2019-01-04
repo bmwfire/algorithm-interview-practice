d68266f6f520dd60ccea3a765abd2849db92687f
Brandon Wong <bwong7@hawaii.edu>
2019-01-04 06:31:34 -1000

    14.6

--- epi_judge_python/minimum_distance_3_sorted_arrays.py
+++ epi_judge_python/minimum_distance_3_sorted_arrays.py
@@ -1,9 +1,26 @@
+import bintrees
+
 from test_framework import generic_test
 
 
 def find_closest_elements_in_sorted_arrays(sorted_arrays):
-    # TODO - you fill in here.
-    return 0
+    min_distance_so_far = float('inf')
+    iters = bintrees.RBTree()
+    for idx, sorted_array in enumerate(sorted_arrays):
+        it = iter(sorted_array)
+        first_min = next(it, None)
+        if first_min is not None:
+            iters.insert((first_min, idx), it)
+
+    while True:
+        min_value, min_idx = iters.min_key()
+        max_value = iters.max_key()[0]
+        min_distance_so_far = min(min_distance_so_far, max_value - min_value)
+        it = iters.pop_min()[1]
+        next_min = next(it, None)
+        if next_min is None:
+            return min_distance_so_far
+        iters.insert((next_min, min_idx), it)
 
 
 if __name__ == '__main__':
