fec87c820763b4ad997fc01c01c3d58f5c40dd17
Brandon Wong <bwong7@hawaii.edu>
2019-01-05 10:23:41 -1000

    14.7

--- epi_judge_python/a_b_sqrt2.py
+++ epi_judge_python/a_b_sqrt2.py
@@ -1,9 +1,48 @@
+import math
+import bintrees
+
 from test_framework import generic_test
 
 
+class Number:
+    def __init__(self, a, b):
+        self.a, self.b = a, b
+        self.val = a + b * math.sqrt(2)
+
+    def __lt__(self, other):
+        return self.val < other.val
+
+    def __eq__(self, other):
+        return self.val == other.val
+
+
+# Time complexity: O(k log k)
+# Space complexity: O(k)
 def generate_first_k_a_b_sqrt2(k):
-    # TODO - you fill in here.
-    return []
+    candidates = bintrees.RBTree([(Number(0, 0), None)])
+
+    result = []
+    while len(result) < k:
+        next_smallest = candidates.pop_min()[0]
+        result.append(next_smallest.val)
+        candidates[Number(next_smallest.a + 1, next_smallest.b)] = None
+        candidates[Number(next_smallest.a, next_smallest.b + 1)] = None
+    return result
+
+
+# Time complexity: O(n)
+# def generate_first_k_a_b_sqrt2(k):
+#     cand = [Number(0, 0)]
+#     i = j = 0
+#     for _ in range(1, k):
+#         cand_i_plus_1 = Number(cand[i].a + 1, cand[i].b)
+#         cand_j_plus_sqrt2 = Number(cand[j].a, cand[j].b + 1)
+#         cand.append(min(cand_i_plus_1, cand_j_plus_sqrt2))
+#         if cand_i_plus_1.val == cand[-1].val:
+#             i += 1
+#         if cand_j_plus_sqrt2.val == cand[-1].val:
+#             j += 1
+#     return [a.val for a in cand]
 
 
 if __name__ == '__main__':
