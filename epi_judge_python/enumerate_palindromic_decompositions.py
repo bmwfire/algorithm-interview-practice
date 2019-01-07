6cb8e3964b3a9772716c67f941370d02f7fbcb99
Brandon Wong <bwong7@hawaii.edu>
2019-01-07 05:11:14 -1000

    15.7

--- epi_judge_python/enumerate_palindromic_decompositions.py
+++ epi_judge_python/enumerate_palindromic_decompositions.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def palindrome_decompositions(input):
-    # TODO - you fill in here.
-    return []
+    def directed_palindrome_decompositions(offset, partial_partition):
+        if offset == len(input):
+            result.append(list(partial_partition))
+            return
+
+        for i in range(offset + 1, len(input) + 1):
+            prefix = input[offset:i]
+            if prefix == prefix[::-1]:
+                directed_palindrome_decompositions(i, partial_partition + [prefix])
+
+    result = []
+    directed_palindrome_decompositions(0, [])
+    return result
 
 
 def comp(a, b):
