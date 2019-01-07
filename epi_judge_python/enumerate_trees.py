06a46d0e7ecbf4741c8fc03d1c395636a622a4cb
Brandon Wong <bwong7@hawaii.edu>
2019-01-07 05:24:00 -1000

    15.8

--- epi_judge_python/enumerate_trees.py
+++ epi_judge_python/enumerate_trees.py
@@ -1,12 +1,22 @@
 import functools
 
+from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
 def generate_all_binary_trees(num_nodes):
-    # TODO - you fill in here.
-    return []
+    if num_nodes == 0:
+        return [None]
+
+    result = []
+    for num_left_tree_nodes in range(num_nodes):
+        num_right_tree_nodes = num_nodes - 1 - num_left_tree_nodes
+        left_subtrees = generate_all_binary_trees(num_left_tree_nodes)
+        right_subtrees = generate_all_binary_trees(num_right_tree_nodes)
+        result += [BinaryTreeNode(0, left, right) for left in left_subtrees for right in right_subtrees]
+
+    return result
 
 
 def serialize_structure(tree):
