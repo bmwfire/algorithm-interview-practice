ec80cff718381f531ad3a3e68f784969d71db803
Brandon Wong <bwong7@hawaii.edu>
2019-01-07 04:07:03 -1000

    15.4

--- epi_judge_python/power_set.py
+++ epi_judge_python/power_set.py
@@ -1,9 +1,35 @@
+import math
+
 from test_framework import generic_test, test_utils
 
 
+# Non-recursive implementation using bit arrays
 def generate_power_set(S):
-    # TODO - you fill in here.
-    return []
+    power_set = []
+    for int_for_subset in range(1 << len(S)):
+        bit_array = int_for_subset
+        subset = []
+        while bit_array:
+            subset.append(int(math.log2(bit_array & ~(bit_array - 1))))
+            bit_array &= bit_array - 1
+        power_set.append(subset)
+    return power_set
+
+
+# Recursive implementation
+# def generate_power_set(input_set):
+#     def directed_power_set(to_be_selected, selected_so_far):
+#         if to_be_selected == len(input_set):
+#             power_set.append(list(selected_so_far))
+#             return
+#
+#         directed_power_set(to_be_selected + 1, selected_so_far)
+#         directed_power_set(to_be_selected + 1,
+#                            selected_so_far + [input_set[to_be_selected]])
+#
+#     power_set = []
+#     directed_power_set(0, [])
+#     return power_set
 
 
 if __name__ == '__main__':
