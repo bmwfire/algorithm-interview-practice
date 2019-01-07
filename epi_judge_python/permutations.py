e0d160a2d74a97808f2bf44877d7495a9ab7e75a
Brandon Wong <bwong7@hawaii.edu>
2019-01-07 03:42:23 -1000

    15.3

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -2,8 +2,19 @@ from test_framework import generic_test, test_utils
 
 
 def permutations(A):
-    # TODO - you fill in here.
-    return []
+    def directed_permutations(i):
+        if i == len(A) - 1:
+            result.append(A.copy())
+            return
+
+        for j in range(i, len(A)):
+            A[i], A[j] = A[j], A[i]
+            directed_permutations(i + 1)
+            A[i], A[j] = A[j], A[i]
+
+    result = []
+    directed_permutations(0)
+    return result
 
 
 if __name__ == '__main__':
