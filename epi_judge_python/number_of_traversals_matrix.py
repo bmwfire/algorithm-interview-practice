ca94fb2bd76f8cb67448c9d469ad7307bad1e4c0
Brandon Wong <bwong7@hawaii.edu>
2019-01-22 07:37:00 -1000

    16.3

--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -2,8 +2,18 @@ from test_framework import generic_test
 
 
 def number_of_ways(n, m):
-    # TODO - you fill in here.
-    return 0
+    def computer_number_of_ways_to_xy(x, y):
+        if x == y == 0:
+            return 1
+
+        if number_of_ways[x][y] == 0:
+            ways_top = 0 if x == 0 else computer_number_of_ways_to_xy(x - 1, y)
+            ways_left = 0 if y == 0 else computer_number_of_ways_to_xy(x, y - 1)
+            number_of_ways[x][y] = ways_top + ways_left
+        return number_of_ways[x][y]
+
+    number_of_ways = [[0] * m for _ in range(n)]
+    return computer_number_of_ways_to_xy(n - 1, m - 1)
 
 
 if __name__ == '__main__':
