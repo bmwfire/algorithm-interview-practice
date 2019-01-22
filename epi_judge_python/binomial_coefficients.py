fa1ac4163c4e321b5ec980e4a1d4e296d5ba0240
Brandon Wong <bwong7@hawaii.edu>
2019-01-22 08:00:01 -1000

    16.4

--- epi_judge_python/binomial_coefficients.py
+++ epi_judge_python/binomial_coefficients.py
@@ -2,8 +2,18 @@ from test_framework import generic_test
 
 
 def compute_binomial_coefficient(n, k):
-    # TODO - you fill in here.
-    return 0
+    def compute_x_choose_y(x, y):
+        if y in (0, x):
+            return 1
+
+        if x_choose_y[x][y] == 0:
+            without_y = compute_x_choose_y(x - 1, y)
+            with_y = compute_x_choose_y(x - 1, y - 1)
+            x_choose_y[x][y] = without_y + with_y
+        return x_choose_y[x][y]
+
+    x_choose_y = [[0] * (k + 1) for _ in range(n + 1)]
+    return compute_x_choose_y(n, k)
 
 
 if __name__ == '__main__':
