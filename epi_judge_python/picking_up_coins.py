ebd0fff5ab1c04e48a71ad222721fb3e7eb4633b
Brandon Wong <bwong7@hawaii.edu>
2019-01-28 02:27:28 -1000

    16.9

--- epi_judge_python/picking_up_coins.py
+++ epi_judge_python/picking_up_coins.py
@@ -2,8 +2,22 @@ from test_framework import generic_test
 
 
 def maximum_revenue(coins):
-    # TODO - you fill in here.
-    return 0
+    def compute_maximum_revenue_for_range(a, b):
+        if a > b:
+            return 0
+
+        if maximum_revenue_for_range[a][b] == 0:
+            max_revenue_a = coins[a] + min(
+                compute_maximum_revenue_for_range(a + 2, b),
+                compute_maximum_revenue_for_range(a + 1, b - 1))
+            max_revenue_b = coins[b] + min(
+                compute_maximum_revenue_for_range(a + 1, b - 1),
+                compute_maximum_revenue_for_range(a, b - 2))
+            maximum_revenue_for_range[a][b] = max(max_revenue_a, max_revenue_b)
+        return maximum_revenue_for_range[a][b]
+
+    maximum_revenue_for_range = [[0] * len(coins) for _ in coins]
+    return compute_maximum_revenue_for_range(0, len(coins) - 1)
 
 
 if __name__ == '__main__':
