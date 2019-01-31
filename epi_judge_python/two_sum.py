4d978bea9d5140abe7d658ff13da438095dbe4a7
Brandon Wong <bwong7@hawaii.edu>
2019-01-31 09:44:44 -1000

    Invariants boot camp

--- epi_judge_python/two_sum.py
+++ epi_judge_python/two_sum.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def has_two_sum(A, t):
-    # TODO - you fill in here.
-    return True
+    i, j = 0, len(A) - 1
+
+    while i <= j:
+        if A[i] + A[j] == t:
+            return True
+        elif A[i] + A[j] < t:
+            i += 1
+        else:
+            j -= 1
+
+    return False
 
 
 if __name__ == '__main__':
