4c6530e358d4cf06c7a390eb45a99fc25cb2e5f3
Brandon Wong <bwong7@hawaii.edu>
2019-02-04 09:15:23 -1000

    17.8

--- epi_judge_python/largest_rectangle_under_skyline.py
+++ epi_judge_python/largest_rectangle_under_skyline.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def calculate_largest_rectangle(heights):
-    # TODO - you fill in here.
-    return 0
+    pillar_indices, max_rectangle_area = [], 0
+    for i, h in enumerate(heights + [0]):
+        while pillar_indices and heights[pillar_indices[-1]] >= h:
+            height = heights[pillar_indices.pop()]
+            width = i if not pillar_indices else i - pillar_indices[-1] - 1
+            max_rectangle_area = max(max_rectangle_area, height * width)
+        pillar_indices.append(i)
+    return max_rectangle_area
 
 
 if __name__ == '__main__':
