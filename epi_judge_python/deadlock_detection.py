7dfe4a4a901e1297cf5dca6a81ef3981d3ebaaa9
Brandon Wong <bwong7@hawaii.edu>
2019-03-17 21:58:44 -1000

    18.4

--- epi_judge_python/deadlock_detection.py
+++ epi_judge_python/deadlock_detection.py
@@ -5,13 +5,27 @@ from test_framework.test_utils import enable_executor_hook
 
 
 class GraphVertex:
+
+    WHITE, GRAY, BLACK = range(3)
+
+
     def __init__(self):
+        self.color = GraphVertex.WHITE
         self.edges = []
 
 
 def is_deadlocked(graph):
-    # TODO - you fill in here.
-    return True
+    def has_cycle(cur):
+        if cur.color == GraphVertex.GRAY:
+            return True
+
+        cur.color = GraphVertex.GRAY
+        if any(next.color != GraphVertex.BLACK and has_cycle(next) for next in cur.edges):
+            return True
+        cur.color = GraphVertex.BLACK
+        return False
+
+    return any(vertex.color == GraphVertex.WHITE and has_cycle(vertex) for vertex in graph)
 
 
 @enable_executor_hook
