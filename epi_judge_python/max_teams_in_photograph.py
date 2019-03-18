2e3f5b420e03f9bda56439aef7e11755c4386497
Brandon Wong <bwong7@hawaii.edu>
2019-03-18 01:02:23 -1000

    18.8

--- epi_judge_python/max_teams_in_photograph.py
+++ epi_judge_python/max_teams_in_photograph.py
@@ -7,13 +7,17 @@ from test_framework.test_utils import enable_executor_hook
 class GraphVertex:
     def __init__(self):
         self.edges = []
-        # Set max_distance = 0 to indicate unvisitied vertex.
+        # Set max_distance = 0 to indicate unvisited vertex.
         self.max_distance = 0
 
 
 def find_largest_number_teams(graph):
-    # TODO - you fill in here.
-    return 0
+    def dfs(curr):
+        curr.max_distance = max(((vertex.max_distance if vertex.max_distance != 0 else dfs(vertex)) + 1
+                                 for vertex in curr.edges), default=1)
+        return curr.max_distance
+
+    return max(dfs(g) for g in graph if g.max_distance == 0)
 
 
 @enable_executor_hook
