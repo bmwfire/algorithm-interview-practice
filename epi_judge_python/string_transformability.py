526d4f0de0446bdc1f1e7b77c21c814c13a7f43d
Brandon Wong <bwong7@hawaii.edu>
2019-03-18 00:18:10 -1000

    18.7

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -1,10 +1,27 @@
+import collections
+import string
+
 from test_framework import generic_test
 
 
 # Uses BFS to find the least steps of transformation.
 def transform_string(D, s, t):
-    # TODO - you fill in here.
-    return 0
+    StringWithDistance = collections.namedtuple('StringWithDistance', ('candidate_string', 'distance'))
+    q = collections.deque([StringWithDistance(s, 0)])
+    D.remove(s)
+
+    while q:
+        f = q.popleft()
+        if f.candidate_string == t:
+            return f.distance
+
+        for i in range(len(f.candidate_string)):
+            for c in string.ascii_lowercase:
+                cand = f.candidate_string[:i] + c + f.candidate_string[i + 1:]
+                if cand in D:
+                    D.remove(cand)
+                    q.append(StringWithDistance(cand, f.distance + 1))
+    return -1
 
 
 if __name__ == '__main__':
