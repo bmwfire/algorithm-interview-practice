52a898ee848fe84dc2e9f0078d597b0738ac552b
Brandon Wong <bwong7@hawaii.edu>
2019-05-01 00:21:09 -1000

    Revert "4.3"
    This reverts commit 774ec2226bc81bf4be455dbd363a734360adc31d.

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -4,11 +4,14 @@ from test_framework import generic_test
 def reverse_bits(x):
 
     result = 0
-    for _ in range(4):
+    for i in range(64):
         result <<= 1
         result |= x & 1
         x >>= 1
     return result
 
 
-print(reverse_bits(11))
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main("reverse_bits.py", "reverse_bits.tsv",
+                                       reverse_bits))
