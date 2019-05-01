774ec2226bc81bf4be455dbd363a734360adc31d
Brandon Wong <bwong7@hawaii.edu>
2019-05-01 00:18:56 -1000

    4.3

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -4,14 +4,11 @@ from test_framework import generic_test
 def reverse_bits(x):
 
     result = 0
-    for i in range(64):
+    for _ in range(4):
         result <<= 1
         result |= x & 1
         x >>= 1
     return result
 
 
-if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("reverse_bits.py", "reverse_bits.tsv",
-                                       reverse_bits))
+print(reverse_bits(11))
