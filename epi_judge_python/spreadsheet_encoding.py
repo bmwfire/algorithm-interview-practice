11b08a99f2e7a379fcf44051516c3d99792bf242
Brandon Wong <bwong7@hawaii.edu>
2019-06-19 11:34:08 -1000

    6.3

--- epi_judge_python/spreadsheet_encoding.py
+++ epi_judge_python/spreadsheet_encoding.py
@@ -7,6 +7,15 @@ def ss_decode_col_id(col):
     return functools.reduce(lambda result, c: result * 26 + ord(c) - ord('A') + 1, col, 0)
 
 
+    # # Non-functional programming solution
+    # result = 0
+    #
+    # for c in col:
+    #     result = result * 26 + ord(c) - ord('A') + 1
+    #
+    # return result
+
+
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main("spreadsheet_encoding.py",
